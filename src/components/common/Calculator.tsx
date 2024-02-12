04a7d5cff0da43b6f0043c805fb82dafbb97567a
LinaYahya <linaebe0@gmail.com>
2024-02-12 17:21:38 +0100

    fix: add svg type and fix calculator effect dependancy

--- src/components/common/Calculator.tsx
+++ src/components/common/Calculator.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import * as math from 'mathjs';
 import _ from 'lodash';
 
@@ -49,150 +49,159 @@ const Calculator = ({ standalone = false }: Props): JSX.Element => {
   const [scientificMode, setScientificMode] = useState(false);
   const [history, setHistory] = useState<string[]>([]);
 
-  const updateResult = ({
-    name,
-    text,
-    katex,
-    mathjs: mathjs2,
-  }: {
-    name: string;
-    text: string;
-    katex?: string;
-    mathjs?: string;
-  }): void => {
-    try {
-      const needReset = [
-        t(RESULT_ERROR_MESSAGE),
-        'undefined',
-        t('Infinity'),
-        t('-Infinity'),
-      ].includes(result);
-      const isNewComputation = !history.length && !OPERATIONS.includes(name);
-      let newResult = needReset ? '' : result;
-      let newMathjs = needReset ? '' : mathjs;
-      let newHistory: string[] = [...history];
-      switch (name) {
-        case BUTTON_NAMES.EE: {
-          const resultAsFloat = parseFloat(compute(newMathjs, t));
-          newMathjs = math.format(resultAsFloat, { notation: 'exponential' });
-          // transform exponential notation XXe+YY to katex XXe^(YY)
-          const matchedMath = newMathjs.match(/[+-](?=[0-9])/);
-          if (matchedMath) {
-            const [sign] = matchedMath;
-            const [number, power] = newMathjs.split(sign);
-            newResult = [
-              number,
-              `{(${sign === '-' ? sign : ''}${power})}`,
-            ].join('^');
+  const updateResult = useCallback(
+    ({
+      name,
+      text,
+      katex,
+      mathjs: mathjs2,
+    }: {
+      name: string;
+      text: string;
+      katex?: string;
+      mathjs?: string;
+    }): void => {
+      try {
+        const needReset = [
+          t(RESULT_ERROR_MESSAGE),
+          'undefined',
+          t('Infinity'),
+          t('-Infinity'),
+        ].includes(result);
+        const isNewComputation = !history.length && !OPERATIONS.includes(name);
+        let newResult = needReset ? '' : result;
+        let newMathjs = needReset ? '' : mathjs;
+        let newHistory: string[] = [...history];
+
+        switch (name) {
+          case BUTTON_NAMES.EE: {
+            const resultAsFloat = parseFloat(compute(newMathjs, t));
+            newMathjs = math.format(resultAsFloat, { notation: 'exponential' });
+            // transform exponential notation XXe+YY to katex XXe^(YY)
+            const matchedMath = newMathjs.match(/[+-](?=[0-9])/);
+            if (matchedMath) {
+              const [sign] = matchedMath;
+              const [number, power] = newMathjs.split(sign);
+              newResult = [
+                number,
+                `{(${sign === '-' ? sign : ''}${power})}`,
+              ].join('^');
+              newHistory = [];
+            }
+            break;
+          }
+          case BUTTON_NAMES.FACTORIAL:
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            newMathjs = math.factorial(compute(newResult, t) as any).toString();
+            newResult = newMathjs;
             newHistory = [];
-          }
-          break;
-        }
-        case BUTTON_NAMES.FACTORIAL:
-          // eslint-disable-next-line @typescript-eslint/no-explicit-any
-          newMathjs = math.factorial(compute(newResult, t) as any).toString();
-          newResult = newMathjs;
-          newHistory = [];
-          break;
+            break;
 
-        case BUTTON_NAMES.SQRT:
-          newMathjs = compute(`sqrt(${newMathjs})`, t);
-          newResult = newMathjs;
-          newHistory = [];
-          break;
-        case BUTTON_NAMES.EQUAL:
-          newMathjs = compute(newMathjs, t);
-          newResult = newMathjs;
-          newHistory = [];
-          break;
-        case BUTTON_NAMES.ABS:
-          newMathjs = compute(`abs(${newMathjs})`, t);
-          newResult = newMathjs;
-          newHistory = [];
-          break;
-        case BUTTON_NAMES.CLEAR:
-          newMathjs = '';
-          newResult = '';
-          newHistory = [];
-          break;
-        case BUTTON_NAMES.CE:
-          [newResult, newMathjs] = backSpace(
-            newResult,
-            newMathjs,
-            newHistory,
-            t,
-          );
-          break;
-        case BUTTON_NAMES.PI: {
-          // start new computation at the end of previous computation
-          // pi acts as a number, so it should start a new computation
-          // after an equal operation
-          newResult = isNewComputation ? '' : newResult;
-          newMathjs = isNewComputation ? '' : newMathjs;
+          case BUTTON_NAMES.SQRT:
+            newMathjs = compute(`sqrt(${newMathjs})`, t);
+            newResult = newMathjs;
+            newHistory = [];
+            break;
+          case BUTTON_NAMES.EQUAL:
+            newMathjs = compute(newMathjs, t);
+            newResult = newMathjs;
+            newHistory = [];
+            break;
+          case BUTTON_NAMES.ABS:
+            newMathjs = compute(`abs(${newMathjs})`, t);
+            newResult = newMathjs;
+            newHistory = [];
+            break;
+          case BUTTON_NAMES.CLEAR:
+            newMathjs = '';
+            newResult = '';
+            newHistory = [];
+            break;
+          case BUTTON_NAMES.CE:
+            [newResult, newMathjs] = backSpace(
+              newResult,
+              newMathjs,
+              newHistory,
+              t,
+            );
+            break;
+          case BUTTON_NAMES.PI: {
+            // start new computation at the end of previous computation
+            // pi acts as a number, so it should start a new computation
+            // after an equal operation
+            newResult = isNewComputation ? '' : newResult;
+            newMathjs = isNewComputation ? '' : newMathjs;
 
-          // we add a times operation if the last entry is a number or pi
-          const lastCharacter = newResult.slice(-1);
-          const addTimes =
-            !_.isNaN(parseInt(lastCharacter, 10)) ||
-            lastCharacter === PI_SYMBOL;
-          newResult += addTimes ? `${TIMES_SYMBOL}${PI_SYMBOL}` : PI_SYMBOL;
-          newMathjs += addTimes ? `*${mathjs2}` : mathjs2;
-          if (addTimes) {
-            newHistory.push(BUTTON_NAMES.MULTIPLY);
-          }
-          newHistory.push(name);
-          break;
-        }
-        case BUTTON_NAMES.TOGGLE_SIGN: {
-          const toggledResult = toggleSign(newResult);
-          const toggledMathjs = toggleSign(newMathjs);
-
-          // remember operation only on change
-          if (newResult !== toggledResult || newMathjs !== toggledMathjs) {
-            newMathjs = toggledMathjs || '';
-            newResult = toggledResult || '';
+            // we add a times operation if the last entry is a number or pi
+            const lastCharacter = newResult.slice(-1);
+            const addTimes =
+              !_.isNaN(parseInt(lastCharacter, 10)) ||
+              lastCharacter === PI_SYMBOL;
+            newResult += addTimes ? `${TIMES_SYMBOL}${PI_SYMBOL}` : PI_SYMBOL;
+            newMathjs += addTimes ? `*${mathjs2}` : mathjs2;
+            if (addTimes) {
+              newHistory.push(BUTTON_NAMES.MULTIPLY);
+            }
             newHistory.push(name);
+            break;
           }
-          break;
+          case BUTTON_NAMES.TOGGLE_SIGN: {
+            const toggledResult = toggleSign(newResult);
+            const toggledMathjs = toggleSign(newMathjs);
+
+            // remember operation only on change
+            if (newResult !== toggledResult || newMathjs !== toggledMathjs) {
+              newMathjs = toggledMathjs || '';
+              newResult = toggledResult || '';
+              newHistory.push(name);
+            }
+            break;
+          }
+          default:
+            // start new computation at the end of previous computation
+            newResult = isNewComputation ? '' : newResult;
+            newMathjs = isNewComputation ? '' : newMathjs;
+
+            newResult += katex || text;
+            newMathjs += mathjs2 || text;
+            newHistory.push(name);
+            break;
         }
-        default:
-          // start new computation at the end of previous computation
-          newResult = isNewComputation ? '' : newResult;
-          newMathjs = isNewComputation ? '' : newMathjs;
 
-          newResult += katex || text;
-          newMathjs += mathjs2 || text;
-          newHistory.push(name);
-          break;
+        setResult(newResult);
+        setMathjs(newMathjs);
+        setHistory(newHistory);
+      } catch (e) {
+        console.error(e, 'error');
+        setResult(t(RESULT_ERROR_MESSAGE) || 'Error');
+        setMathjs(t(RESULT_ERROR_MESSAGE) || 'Error');
+        setHistory([]);
       }
+    },
+    [history, mathjs, result, t],
+  );
 
-      setResult(newResult);
-      setMathjs(newMathjs);
-      setHistory(newHistory);
-    } catch (e) {
-      console.error(e, 'error');
-      setResult(t(RESULT_ERROR_MESSAGE) || 'Error');
-      setMathjs(t(RESULT_ERROR_MESSAGE) || 'Error');
-      setHistory([]);
-    }
-  };
+  const handleKeydown = useCallback(
+    (event: KeyboardEvent): void => {
+      const { key } = event;
 
-  const handleKeydown = (event: KeyboardEvent): void => {
-    const { key } = event;
-
-    const buttonName = getButtonName(key);
-    if (buttonName) {
-      const button = KEYPAD_BUTTONS.find(({ name }) => name === buttonName);
-      if (button) {
-        updateResult(button);
+      const buttonName = getButtonName(key);
+      if (buttonName) {
+        const button = KEYPAD_BUTTONS.find(({ name }) => name === buttonName);
+        if (button) {
+          updateResult(button);
+        }
       }
-    }
-  };
+    },
+    [updateResult],
+  );
 
   useEffect(() => {
     document.addEventListener('keydown', handleKeydown);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+    return () => {
+      document.removeEventListener('keydown', handleKeydown);
+    };
+  }, [handleKeydown]);
 
   useEffect(() => {
     updateAngleUnit(angleUnit);
